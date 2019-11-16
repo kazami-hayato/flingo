fec57b48f5208fc6a2a09cda7cc56ae4832a99fe
934991794 <934991794@qq.com>
2019-11-16 15:01:38 +0800

    init

---
+++ src/main/java/com/uvt/flingo/FlingoApplication.java
@@ -0,0 +1,13 @@
+package com.uvt.flingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlingoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlingoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uvt/flingo/FlingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uvt.flingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlingoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
